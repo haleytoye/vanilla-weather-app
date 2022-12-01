ffb240061122490bdcd33e349cbccf3979911fe7
haleytoye <hjtoye@gmail.com>
2022-12-01 14:13:14 -0500

    showing current date and time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   console.log(response);
   console.log(response.data.temperature.current);
@@ -7,6 +26,7 @@ function displayTemperature(response) {
   let descEl = document.querySelector("#description");
   let humidityEl = document.querySelector("#humidity");
   let windEl = document.querySelector("#wind");
+  let dateEl = document.querySelector("#date");
 
   cityEl.innerHTML = response.data.city;
   tempEl.innerHTML = Math.round(response.data.temperature.current);
@@ -14,6 +34,8 @@ function displayTemperature(response) {
   descEl.innerHTML = response.data.condition.description;
   humidityEl.innerHTML = response.data.temperature.humidity;
   windEl.innerHTML = Math.round(response.data.wind.speed);
+
+  dateEl.innerHTML = formatDate(response.data.time * 1000);
 }
 let apikey = "1o0f4aea9af54436031a3t4f3b2ca21f";
 let units = "metric";
