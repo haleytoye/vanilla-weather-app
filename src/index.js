833e3f69d270a96e1897358f2fd5e6426e9239c3
haleytoye <hjtoye@gmail.com>
2022-12-01 18:51:13 -0500

    removed current location due to api restrictions

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ function displayTemperature(response) {
   let tempEl = document.querySelector("#temperature");
   let feelsEl = document.querySelector("#feels-like");
   let cityEl = document.querySelector("#city");
+  let cityInput = document.querySelector("#city-input");
   let descEl = document.querySelector("#description");
   let humidityEl = document.querySelector("#humidity");
   let windEl = document.querySelector("#wind");
@@ -30,7 +31,7 @@ function displayTemperature(response) {
   let iconEl = document.querySelector("#icon");
 
   celsiusTemperature = response.data.temperature.current;
-
+  cityInput.value = "";
   cityEl.innerHTML = response.data.city;
   tempEl.innerHTML = Math.round(response.data.temperature.current);
   feelsEl.innerHTML = Math.round(response.data.temperature.feels_like);
